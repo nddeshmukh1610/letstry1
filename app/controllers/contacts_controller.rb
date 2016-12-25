cdccea6438f265255425ff224235dccc0ea1693c
Ubuntu <ubuntu@ip-172-31-46-171.us-west-2.compute.internal>
2016-12-25 11:52:07 +0000

    contact invalid id

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,5 @@
 class ContactsController < InheritedResources::Base
+	rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def new 
 		@contact = Contact.new			
@@ -14,6 +15,10 @@ class ContactsController < InheritedResources::Base
 	end
 
   private
+
+    def record_not_found
+		redirect_to root_path
+    end
 
     def contact_params
       params.require(:contact).permit(:contactperson, :contactemail, :contactno, :company, :subject, :description)
