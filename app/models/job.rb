25a4c0399f3a94302424133436ff52e59744b747
Ubuntu <ubuntu@ip-172-31-46-171.us-west-2.compute.internal>
2016-12-31 15:39:08 +0000

    gui change and sanatize

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,13 +8,13 @@ class Job < ActiveRecord::Base
 	belongs_to :japproval
 	
 	
-	validates :title, presence:true, length: {minimum:2, maximum:40}
-	validates :company, presence:true, length: {minimum:3, maximum:30}
-	validates :comurl, presence:true, length: {minimum:4, maximum:30}
+	validates :title, presence:true, length: {minimum:2, maximum:50}
+	validates :company, presence:true, length: {minimum:3, maximum:50}
+	validates :comurl, presence:true, length: {minimum:4, maximum:50}
 	validates :minexp, presence:true, numericality: true, length: {minimum:1, maximum:2}
 	validates :maxexp, presence:true, numericality: true, length: {minimum:1, maximum:2}
 	validates :nperiod, presence:true, numericality: true, length: {minimum:1, maximum:2}
-	validates :contactperson, presence:true, length: {minimum:3, maximum:30}
+	validates :contactperson, presence:true, length: {minimum:3, maximum:50}
 	
 	validates :category, presence:true
 	validates :location, presence:true
@@ -23,6 +23,6 @@ class Job < ActiveRecord::Base
 	VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i	#         /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/+\z/i
 	validates :contactemail, presence:true, format: { with: VALID_EMAIL_REGEX }, length: {minimum:6, maximum:30}
 	validates :contactno, presence:true, numericality: true, length: {minimum:10, maximum:13}   # Need to create logic ,only number and + sign
-	validates :description, presence:true, length: {minimum:10, maximum:3000}
+	validates :description, presence:true, length: {minimum:10, maximum:5000}
 	
 end
